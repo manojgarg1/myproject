535bbecb3db7bbf1b41af61250490d7e4de9bb81
Manoj Garg <garg.blr@gmail.com>
2020-03-29 18:58:39 +0530

    Add project files.

---
+++ boards/tests.py
@@ -0,0 +1,41 @@
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+
+Replace this with more appropriate tests for your application.
+"""
+
+import django
+from django.test import TestCase
+from django.urls import reverse
+from django.urls import resolve
+from .views import home
+# TODO: Configure your database in settings.py and sync before running tests.
+
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
+
+
+class SimpleTest(TestCase):
+    """Tests for the application views."""
+
+    # Django requires an explicit setup() when running tests in PTVS
+    @classmethod
+    def setUpClass(cls):
+        super(SimpleTest, cls).setUpClass()
+        django.setup()
+
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
